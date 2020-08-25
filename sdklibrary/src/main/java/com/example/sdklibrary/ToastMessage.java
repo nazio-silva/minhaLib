7414cbe41f134b13b82e8342d93415f4ecc654b2
Nazio Silva <nazio.silva@gmail.com>
2020-08-25 10:38:47 -0300

    Criando minha primeira biblioteca nativa.

---
+++ sdklibrary/src/main/java/com/example/sdklibrary/ToastMessage.java
@@ -0,0 +1,10 @@
+package com.example.sdklibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastMessage {
+    public static void s(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+}
